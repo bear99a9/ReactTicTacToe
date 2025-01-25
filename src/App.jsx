fa0910097e594196b1acc379832d72c487480ff0
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-25 11:27:33 +0000

    Added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./Components/Header/Header";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <>
+      <Header />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <img src="../../../public/game-logo.png" alt="Website logo" />
+      <h1>React noughts and crosses</h1>
+    </header>
+  );
+}
