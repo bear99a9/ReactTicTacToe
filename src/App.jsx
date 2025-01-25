0b900b2cf57b3d65d178f3f1e96337feb86a3eee
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-25 11:32:24 +0000

    None dynamic player section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ function App() {
   return (
     <>
       <Header />
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <li>
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </li>
+            <li>
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </li>
+          </ol>
+          GameBoard
+        </div>
+        Log
+      </main>
     </>
   );
 }
