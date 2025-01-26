2585f6296582d50a097abb0918f69905aa9669a1
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-26 16:37:15 +0000

    Refactor Log component to use map for rendering turns

--- src/Components/log.jsx
+++ src/Components/log.jsx
@@ -1,15 +1,11 @@
 export default function Log({ turns }) {
-  let log = [];
-
-  for (let i = 0; i < turns.length; i++) {
-    const { square, player } = turns[i];
-    const { row, col } = square;
-    log.push(
-      <li key={i}>
-        {player} selected {row}, {col}
-      </li>
-    );
-  }
-
-  return <ol id="log">{log}</ol>;
+  return (
+    <ol id="log">
+      {turns.map((turns) => {
+        <li key={`${turns.square.row}-${turns.square.col}`}>
+          {turns.player} selected {turns.square.row}, {turns.square.col}
+        </li>;
+      })}
+    </ol>
+  );
 }
