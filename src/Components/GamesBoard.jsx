5d4f5fe2dfc778bc747a85b282098961fb0f1249
Sean Edwards <sean.edwards@houseful.co.uk>
2025-01-26 11:33:45 +0000

    Updating object state for immutably / 2D arrays

--- src/Components/GamesBoard.jsx
+++ src/Components/GamesBoard.jsx
@@ -1,3 +1,5 @@
+import React, { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,15 +7,31 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handlePlay(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      let updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ]; // in javascript, this is the recommend way to create a deep copy of a 2D array avoid mutating the original array
+
+      updatedGameBoard[rowIndex][colIndex] = "X";
+
+      return updatedGameBoard;
+    });
+  }
+
   return (
     <>
       <ol id="game-board">
-        {initialGameBoard.map((row, rowIndex) => (
+        {gameBoard.map((row, rowIndex) => (
           <li key={rowIndex}>
             <ol>
               {row.map((playSymbol, colIndex) => (
                 <li key={colIndex}>
-                  <button>{playSymbol}</button>
+                  <button onClick={() => handlePlay(rowIndex, colIndex)}>
+                    {playSymbol}
+                  </button>
                 </li>
               ))}
             </ol>
